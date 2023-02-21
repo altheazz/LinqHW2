e3e7caa4f8d26ce72d31f1475712d9a8b688efe3
althea <azhong531@gmail.com>
2023-02-21 14:22:28 +0800

    加入專案檔案。

---
+++ LinqHW2_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqHW2_1
+{
+    internal class Program
+    {
+        static void DisplayArray(string[] arr) => Console.WriteLine(string.Join(" ", arr));
+        static void ChangeArray(string[] arr) => Array.Reverse(arr);
+        static void Main(string[] args)
+        {
+            //string[] num = new string[10];
+            Console.WriteLine("輸入數字列，並用逗號隔開");
+            //for(int i = 0; i< 5;i++)
+            //{
+            //    string x = Convert.ToString(Console.ReadLine());
+            //    num[i] = x;
+            //}
+
+            string x = Convert.ToString(Console.ReadLine());
+
+            string[] x_result = x.Split(',');
+
+
+
+            ChangeArray(x_result);
+            Console.WriteLine("倒序");
+            DisplayArray(x_result);
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ LinqHW2_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqHW2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入數字列，並用逗號隔開");
+            //string x = Convert.ToString(Console.ReadLine());
+            string[] x_result = Console.ReadLine().Split(',');
+
+            var odd = x_result.Where((x) => int.Parse(x) % 2 != 0);//奇
+            var even = x_result.Where((x) => int.Parse(x) % 2 == 0);//偶
+
+            Console.WriteLine($"奇數：{string.Join(",",odd)}");
+            Console.WriteLine($"偶數：{string.Join(",", even)}");
+        }
+    }
+}
